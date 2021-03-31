4f69b02c64f2a4734eccf85f4243fe7056b0f184
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-03-31 18:50:03 -0500

    iitial user prompts

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,44 @@
 // Assignment code here
 
+var generatePassword = function() {
+
+    var passLength = window.prompt("Please enter a number between 8 and 128.");
+      localStorage.setItem("length", passLength);
+      if (passLength < 8 || passLength > 128) {
+        alert("Please enter a valid number between 8 and 128.");
+      }
+    var upperCase = window.confirm("Would you like to include uppercase characters?");
+      if (upperCase === true) {
+        upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        localStorage.setItem("uppercase", upperCase);
+      } else {
+        upperCase = null;
+      }
+    var lowerCase = window.confirm("Would you like to include lowercase characters?");
+      if (lowerCase === true) {
+        lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        localStorage.setItem("lowercase", lowerCase);
+      } else {
+        lowerCase = null;
+      }
+    var specialChar = window.confirm("Would you like to utilize special characters?");
+      if (specialChar === true) {
+        specialChar = "!@#$%&?";
+        localStorage.setItem("specialchar", specialChar);
+      } else {
+        specialChar = null;
+      }
+    var numChar = window.confirm("Would you like to use numbers in your password?")
+    if (numChar === true) {
+      numChar = "1234567890";
+      localStorage.setItem("numbers", numChar);
+    } else {
+      numChar = "";
+    }
+    };
+
+localStorage.clear("key");
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
