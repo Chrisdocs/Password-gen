9c8baaf08dffd2b4e2fd5c656d7f263abd752de8
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-01 15:46:07 -0500

    found some code through google search, modified and added it to get desired result

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,43 +1,56 @@
 // Assignment code here
-
 var generatePassword = function() {
+  
+  passwordLength = window.prompt("Please enter a number between 8 and 128.");
+  localStorage.setItem("length", passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid number between 8 and 128.");
+  }
 
-    var passLength = window.prompt("Please enter a number between 8 and 128.");
-      localStorage.setItem("length", passLength);
-      if (passLength < 8 || passLength > 128) {
-        alert("Please enter a valid number between 8 and 128.");
-      }
-    var upperCase = window.confirm("Would you like to include uppercase characters?");
-      if (upperCase === true) {
-        upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        localStorage.setItem("uppercase", upperCase);
-      } else {
-        upperCase = null;
-      }
-    var lowerCase = window.confirm("Would you like to include lowercase characters?");
-      if (lowerCase === true) {
-        lowerCase = "abcdefghijklmnopqrstuvwxyz";
-        localStorage.setItem("lowercase", lowerCase);
-      } else {
-        lowerCase = null;
-      }
-    var specialChar = window.confirm("Would you like to utilize special characters?");
-      if (specialChar === true) {
-        specialChar = "!@#$%&?";
-        localStorage.setItem("specialchar", specialChar);
-      } else {
-        specialChar = null;
-      }
-    var numChar = window.confirm("Would you like to use numbers in your password?")
+  numChar = window.confirm("Would you like to use numbers in your password?")
     if (numChar === true) {
       numChar = "1234567890";
-      localStorage.setItem("numbers", numChar);
     } else {
       numChar = "";
     }
-    };
 
-localStorage.clear("key");
+  upperCase = window.confirm("Would you like to include uppercase characters?");
+    if (upperCase === true) {
+      upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } else {
+      upperCase = null;
+    }
+
+  lowerCase = window.confirm("Would you like to include lowercase characters?");
+    if (lowerCase === true) {
+      lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    } else {
+      lowerCase = null;
+    }
+
+  specialChar = window.confirm("Would you like to utilize special characters?");
+    if (specialChar === true) {
+      specialChar = "!@#$%&?";
+    } else {
+      specialChar = null;
+    }
+
+    var allChars = numChar + upperCase + lowerCase + specialChar
+    var randPasswordArray = Array(passwordLength);
+    randPasswordArray[passwordLength] = "";
+    randPasswordArray = randPasswordArray.fill(allChars, "");
+    return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
+  }
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
 
 
 // Get references to the #generate element
