6320e3eebff99f311c3fe660424e6a0cda65eae6
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-01 16:07:23 -0500

    some documentation for reading and understanding the code in .js

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Password Generator Starter Code
+# Password genertator
+
+## Purpose
+A website twhich provides the user with random password generation based on the parameters provided from a series of prompts
+
+## Built With
+* HTML
+* CSS
+* Javascript
+
+## Website
+
+
+## Contribution
+Made by Chris Wolfe
+
+
+
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,16 @@
 // Assignment code here
+
+//calls the function when the generate password button is clicked
 var generatePassword = function() {
   
+  //prompts the user to provide a password length between 8 and 128
   passwordLength = window.prompt("Please enter a number between 8 and 128.");
   localStorage.setItem("length", passwordLength);
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid number between 8 and 128.");
   }
 
+  //prompts the user to confirm if they would like to use numbers
   numChar = window.confirm("Would you like to use numbers in your password?")
     if (numChar === true) {
       numChar = "1234567890";
@@ -14,6 +18,7 @@ var generatePassword = function() {
       numChar = "";
     }
 
+    //promtps the user is they would like to use upper case letters
   upperCase = window.confirm("Would you like to include uppercase characters?");
     if (upperCase === true) {
       upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -21,6 +26,7 @@ var generatePassword = function() {
       upperCase = null;
     }
 
+    //promtps the user if they would like to use lower case letters
   lowerCase = window.confirm("Would you like to include lowercase characters?");
     if (lowerCase === true) {
       lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -28,6 +34,7 @@ var generatePassword = function() {
       lowerCase = null;
     }
 
+    //prompts the user if they would like to use special characters
   specialChar = window.confirm("Would you like to utilize special characters?");
     if (specialChar === true) {
       specialChar = "!@#$%&?";
@@ -35,13 +42,19 @@ var generatePassword = function() {
       specialChar = null;
     }
 
+//following code was found and modified from https://www.codegrepper.com/code-examples/javascript/javascript+password+generator+example
+
+    //creates a variable which combines all the user selected inputs into a single string
     var allChars = numChar + upperCase + lowerCase + specialChar
+
+    //takes the allChar variable and applies an array to it which the length is determined by passwordLength
     var randPasswordArray = Array(passwordLength);
     randPasswordArray[passwordLength] = "";
     randPasswordArray = randPasswordArray.fill(allChars, "");
+    //randomizes the order and returns it
     return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
   }
-
+//creates a for loop, not actually totally sure how this works code found here: https://www.codegrepper.com/code-examples/javascript/javascript+password+generator+example
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
